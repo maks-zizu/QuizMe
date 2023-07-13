1f27bb2bd14ecb7a54b7d00c3f2e62aed64abf49
Maks Zhovna <maks.zhovna@gmail.com>
2023-07-13 18:12:15 +0300

    server

--- app.js
+++ app.js
@@ -1,19 +1,20 @@
+require('@babel/register');
+const express = require('express');
+const React = require('react');
+const ReactDOMServer = require('react-dom/server');
+const path = require('path');
+const morgan = require('morgan');
 const { sequelize } = require('./db/models');
 
+const app = express();
+const PORT = 3005;
 
-
-
-
-// ddd
-
-
-
-// app.listen(PORT, async () => {
-//   console.log(`Server started on port ${PORT}`);
-//   try {
-//     await sequelize.authenticate();
-//     console.log('Connection BD has been established successfully.');
-//   } catch (error) {
-//     console.error('Unable to connect to the database:', error);
-//   }
-// });
+app.listen(PORT, async () => {
+  console.log(`Server started on port ${PORT}`);
+  try {
+    await sequelize.authenticate();
+    console.log('Connection BD has been established successfully.');
+  } catch (error) {
+    console.error('Unable to connect to the database:', error);
+  }
+});
